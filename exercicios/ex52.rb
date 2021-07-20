66464f1e974e7028ccea459449a1529a0b96b842
kariane.cunha <karianecoura123@gmail.com>
2021-07-20 14:55:25 -0300

    more

--- exercicios/ex52.rb
+++ exercicios/ex52.rb
@@ -2,10 +2,10 @@ puts "Leia um número:"
 n1=gets.chomp.to_i
 puts "Leia outro número:"
 n2=gets.chomp.to_i
-while n2<n1
+sum=n1+n2
+while n2==n1
     puts "Leia outro número: "
     n2=gets.chomp.to_i
     sum=n1+n2
-
 end
     puts "A soma de ambos valores é: #{sum}"
